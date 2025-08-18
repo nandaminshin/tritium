975a0188594a939a4a86ad69979dbe74d1298f39
Nanda Min Shin <nandaminshin2019@gmail.com>
2025-08-18 16:39:40 +0700

    JWT fix 4

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -40,12 +40,18 @@ const UserController = {
             });
         }
     },
+
     login: async (req, res) => {
         try {
             let { email, password } = req.body;
             let user = await User.login(email, password);
             let token = createJWT(user);
-            res.cookie('jwt', token, { httpOnly: true, maxAge: 3 * 24 * 60 * 60 * 1000 });
+            res.cookie('jwt', token, {
+                httpOnly: true,
+                secure: true,
+                sameSite: "none",
+                maxAge: 3 * 24 * 60 * 60 * 1000
+            });
             return res.json({ user, token });
         } catch (error) {
             return res.status(400).json({
